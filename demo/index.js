5dce1194f92fd607afc26f79d65ffd663d9a05a1
Patrick Lai <patrick.lai@lifeletters.com.au>
2016-11-10 16:51:17 +1100

    Demo set to lazy image

--- demo/index.js
+++ demo/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import Lazyload from '../src/lazy_image.js';
+import LazyImage from '../src/lazy_image.js';
 import style from './style.scss';
 
 let catUrl = "http://thecatapi.com/api/images/get?format=src&type=gif";
@@ -11,7 +11,7 @@ render( <AppContainer>
           <div className={style.trendyBG}>
             <ul className={style.readingsList}>
               { [1,2,3,4].map(function(r,index){
-                  return <Lazyload key={index} src={`${catUrl}&breaker=${index}`} placeholder={placeholder}/>
+                  return <LazyImage key={index} src={`${catUrl}&breaker=${index}`} placeholder={placeholder}/>
                 })
               }
             </ul>
